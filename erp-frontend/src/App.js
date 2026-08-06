6796f8824d83d797fce6839679cbbb324fe59c32
sahibindenbey-create <sahibindenbey@gmail.com>
2026-08-06 17:27:32 +0300

    Add routes for SiparisForm and StokPanel

--- erp-frontend/src/App.js
+++ erp-frontend/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainMenu from "./components/MainMenu";
 import CariForm from "./components/CariForm";
+import SiparisForm from "./components/SiparisForm";
+import StokPanel from "./pages/StokPanel";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
           <Route path="/" element={<MainMenu />} />
           <Route path="/cariler" element={<CariForm />} />
           <Route path="/cari-kart" element={<CariForm />} />
+          <Route path="/siparis" element={<SiparisForm />} />
+          <Route path="/stok" element={<StokPanel />} />
         </Routes>
         
       </div>
