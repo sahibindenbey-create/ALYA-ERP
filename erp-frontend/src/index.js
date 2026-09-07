42be34d3072197eda4d4d579d5438c43df57b221
sahibindenbey-create <sahibindenbey@gmail.com>
2026-09-08 02:12:50 +0300

    feat: send selected company with all axios requests

--- erp-frontend/src/index.js
+++ erp-frontend/src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios';
 import './index.css';
 import './theme.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// Tüm Axios isteklerine aktif şirketi otomatik ekle.
+// Böylece mevcut modüllerin tek tek değiştirilmesine gerek kalmaz.
+axios.interceptors.request.use((config) => {
+  const companyId = localStorage.getItem('selectedCompanyId') || '1';
+  config.headers = config.headers || {};
+  config.headers['X-Company-Id'] = companyId;
+  return config;
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,7 +22,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
