71c6fb94b24fe06eb8a30c6dfbb7f131dc8602d9
sahibindenbey-create <sahibindenbey@gmail.com>
2026-09-09 00:01:50 +0300

    fix: harden company isolation for ERP modules

---
+++ erp-backend/sql/005_company_isolation_hardening.sql
@@ -0,0 +1,125 @@
+/*
+  ALYA ERP - 005 / COMPANY ISOLATION HARDENING
+
+  Amaç:
+  - Şirket bazlı tablolarda CompanyId'nin boş/yanlış kalmasını önlemek.
+  - 002 ve 003 migration'larının kapsamadığı yeni/üretim tablolarını güvenceye almak.
+  - Mevcut RLS politikalarını bozmayıp yalnızca eksik olanları eklemek.
+
+  NOT:
+  Şirket seçimi HTTP X-Company-Id -> SESSION_CONTEXT('CompanyId') üzerinden gelir.
+  Mevcut policy varsa tekrar oluşturulmaz.
+*/
+SET NOCOUNT ON;
+SET XACT_ABORT ON;
+
+DECLARE @Tables TABLE (TableName SYSNAME PRIMARY KEY);
+
+INSERT INTO @Tables(TableName) VALUES
+    (N'CariListesi'),
+    (N'CariHareketleri'),
+    (N'CariEvrak'),
+    (N'Siparisler'),
+    (N'SiparisDetay'),
+    (N'Urunler'),
+    (N'UrunDosya'),
+    (N'Numuneler'),
+    (N'Teklifler'),
+    (N'TeklifKalemleri'),
+    (N'Faturalar'),
+    (N'FaturaDetay'),
+    (N'KasaBanka'),
+    (N'KasaBankaHareketleri'),
+    (N'Personel'),
+    (N'Receteler'),
+    (N'ReceteDetay'),
+    (N'ReceteKalemleri'),
+    (N'ReceteIstasyon'),
+    (N'UretimEmirleri'),
+    (N'FasonIsler'),
+    (N'FasonHareketleri'),
+    (N'Irsaliyeler'),
+    (N'IrsaliyeDetay'),
+    (N'PlatformSiparisler');
+
+DECLARE @TableName SYSNAME;
+DECLARE @sql NVARCHAR(MAX);
+DECLARE @policyName SYSNAME;
+DECLARE @defaultName SYSNAME;
+DECLARE @predicateName SYSNAME = N'fn_CompanyIsolationPredicate';
+
+IF OBJECT_ID(N'dbo.' + @predicateName, N'IF') IS NULL
+    THROW 52001, N'fn_CompanyIsolationPredicate bulunamadı. Önce şirket izolasyonu migration''larını çalıştırın.', 1;
+
+DECLARE c CURSOR LOCAL FAST_FORWARD FOR
+    SELECT TableName FROM @Tables ORDER BY TableName;
+OPEN c;
+FETCH NEXT FROM c INTO @TableName;
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+    IF OBJECT_ID(N'dbo.' + @TableName, N'U') IS NOT NULL
+       AND COL_LENGTH(N'dbo.' + @TableName, N'CompanyId') IS NOT NULL
+    BEGIN
+        /* Yeni kayıtların CompanyId'si boş bırakılırsa aktif şirketten gelsin. */
+        SET @defaultName = N'DF_' + @TableName + N'_CompanyContext';
+
+        IF NOT EXISTS
+        (
+            SELECT 1
+            FROM sys.default_constraints dc
+            WHERE dc.parent_object_id = OBJECT_ID(N'dbo.' + @TableName)
+              AND dc.name = @defaultName
+        )
+        BEGIN
+            SET @sql = N'ALTER TABLE dbo.' + QUOTENAME(@TableName) +
+                       N' ADD CONSTRAINT ' + QUOTENAME(@defaultName) +
+                       N' DEFAULT (ISNULL(TRY_CONVERT(INT, SESSION_CONTEXT(N''CompanyId'')), 1)) FOR CompanyId;';
+            EXEC sys.sp_executesql @sql;
+        END;
+
+        /* Aynı tablo için mevcut bir FILTER policy varsa dokunma. */
+        IF NOT EXISTS
+        (
+            SELECT 1
+            FROM sys.security_predicates sp
+            WHERE sp.target_object_id = OBJECT_ID(N'dbo.' + @TableName)
+              AND sp.predicate_type = 0
+        )
+        BEGIN
+            SET @policyName = N'RLS_AlyaCompany_Hardening_' + @TableName;
+
+            IF NOT EXISTS (SELECT 1 FROM sys.security_policies WHERE name = @policyName)
+            BEGIN
+                SET @sql = N'
+                    CREATE SECURITY POLICY dbo.' + QUOTENAME(@policyName) + N'
+                    ADD FILTER PREDICATE dbo.' + QUOTENAME(@predicateName) + N'(CompanyId)
+                        ON dbo.' + QUOTENAME(@TableName) + N',
+                    ADD BLOCK PREDICATE dbo.' + QUOTENAME(@predicateName) + N'(CompanyId)
+                        ON dbo.' + QUOTENAME(@TableName) + N' AFTER INSERT,
+                    ADD BLOCK PREDICATE dbo.' + QUOTENAME(@predicateName) + N'(CompanyId)
+                        ON dbo.' + QUOTENAME(@TableName) + N' AFTER UPDATE
+                    WITH (STATE = ON);';
+                EXEC sys.sp_executesql @sql;
+            END;
+        END;
+    END;
+
+    FETCH NEXT FROM c INTO @TableName;
+END;
+
+CLOSE c;
+DEALLOCATE c;
+
+/* Kontrol çıktısı: hangi tabloların aktif şirket izolasyonunda olduğunu göster. */
+SELECT
+    OBJECT_SCHEMA_NAME(sp.target_object_id) AS SchemaName,
+    OBJECT_NAME(sp.target_object_id) AS TableName,
+    p.predicate_type_desc AS PredicateType,
+    p.operation_desc AS Operation
+FROM sys.security_predicates p
+INNER JOIN sys.security_policies sp
+    ON sp.object_id = p.object_id
+WHERE sp.is_enabled = 1
+  AND OBJECT_NAME(sp.target_object_id) IN (SELECT TableName FROM @Tables)
+ORDER BY TableName, PredicateType, Operation;
