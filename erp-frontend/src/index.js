9d6eb2b2b51a4aa30bf1aa7e8d270150b613d4e2
sahibindenbey-create <sahibindenbey@gmail.com>
2026-09-09 01:59:21 +0300

    feat: load professional UI styles

--- erp-frontend/src/index.js
+++ erp-frontend/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import axios from 'axios';
 import './index.css';
 import './theme.css';
+import './professional-ui.css';
+import './professional-modules.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
