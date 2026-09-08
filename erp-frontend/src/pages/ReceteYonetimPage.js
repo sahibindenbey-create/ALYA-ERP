3bc35b01a40258fcc6cacc99ba3c7f7d054f2a6b
sahibindenbey-create <sahibindenbey@gmail.com>
2026-09-08 17:46:42 +0300

    AHBRD 1301 ağacını gerçek Reçete Yönetimine bağla

--- erp-frontend/src/pages/ReceteYonetimPage.js
+++ erp-frontend/src/pages/ReceteYonetimPage.js
@@ -1,1 +1,13 @@
-export { default } from './ReceteYonetimPageV3';
+import React from 'react';
+import ReceteYonetimPageV3 from './ReceteYonetimPageV3';
+import Ahbrd1301ReceptTemplate from './Ahbrd1301ReceptTemplate';
+
+export default function ReceteYonetimPage(){
+  const [version,setVersion]=React.useState(0);
+  return <div style={{position:'relative'}} key={version}>
+    <ReceteYonetimPageV3 />
+    <div style={{position:'fixed',right:24,bottom:24,zIndex:2000}}>
+      <Ahbrd1301ReceptTemplate onDone={()=>setVersion(v=>v+1)} />
+    </div>
+  </div>;
+}
