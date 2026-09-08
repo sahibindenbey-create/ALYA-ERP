4db8a66c9eb904e84b3b3aa9a9f2d6f99630c167
sahibindenbey-create <sahibindenbey@gmail.com>
2026-09-08 12:55:31 +0300

    Load company selector theme CSS

--- erp-frontend/src/components/CompanySelector.js
+++ erp-frontend/src/components/CompanySelector.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import "./CompanySelector.css";
 
 const API_URL = "http://localhost:5000/api";
 const STORAGE_KEY = "selectedCompanyId";
