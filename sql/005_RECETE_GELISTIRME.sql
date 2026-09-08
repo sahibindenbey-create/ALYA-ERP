be27eff99dd501d0b8c795fa121c941e9d857e30
sahibindenbey-create <sahibindenbey@gmail.com>
2026-09-08 15:20:13 +0300

    Reçete operasyonlarını da şirket izolasyonuna dahil et

--- sql/005_RECETE_GELISTIRME.sql
+++ sql/005_RECETE_GELISTIRME.sql
@@ -46,9 +46,54 @@ GO
 IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = 'IX_Receteler_Company_Mamul_Aktif' AND object_id = OBJECT_ID('dbo.Receteler'))
     CREATE INDEX IX_Receteler_Company_Mamul_Aktif ON dbo.Receteler(CompanyId, MamulUrunId, IsActive, Versiyon DESC);
 GO
-
 IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = 'IX_ReceteDetay_Company_Recete_Sira' AND object_id = OBJECT_ID('dbo.ReceteDetay'))
     CREATE INDEX IX_ReceteDetay_Company_Recete_Sira ON dbo.ReceteDetay(CompanyId, ReceteId, SiraNo);
+GO
+
+/* ReceteIstasyon eski yapıda şirket alanı yoksa eklenir. */
+IF OBJECT_ID('dbo.ReceteIstasyon','U') IS NOT NULL AND COL_LENGTH('dbo.ReceteIstasyon','CompanyId') IS NULL
+BEGIN
+    ALTER TABLE dbo.ReceteIstasyon ADD CompanyId INT NULL;
+    UPDATE ri SET CompanyId = r.CompanyId FROM dbo.ReceteIstasyon ri JOIN dbo.Receteler r ON r.ReceteId = ri.ReceteId;
+    ALTER TABLE dbo.ReceteIstasyon ADD CONSTRAINT FK_ReceteIstasyon_Sirketler FOREIGN KEY (CompanyId) REFERENCES dbo.Sirketler(CompanyId);
+    ALTER TABLE dbo.ReceteIstasyon ADD CONSTRAINT DF_ReceteIstasyon_CompanyId DEFAULT 1 FOR CompanyId;
+END;
+GO
+
+/* Operasyon tablosunu da aynı RLS politikasına bağla. */
+IF OBJECT_ID('dbo.ReceteIstasyon','U') IS NOT NULL
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1 FROM sys.security_predicates p
+        WHERE p.object_id = OBJECT_ID(N'dbo.SecurityPolicy_CompanyIsolation')
+          AND p.target_object_id = OBJECT_ID(N'dbo.ReceteIstasyon')
+          AND p.predicate_type = 0
+    )
+    BEGIN
+        ALTER SECURITY POLICY dbo.SecurityPolicy_CompanyIsolation
+            ADD FILTER PREDICATE dbo.fn_CompanyIsolationPredicate(CompanyId) ON dbo.ReceteIstasyon;
+    END;
+    IF NOT EXISTS (
+        SELECT 1 FROM sys.security_predicates p
+        WHERE p.object_id = OBJECT_ID(N'dbo.SecurityPolicy_CompanyIsolation')
+          AND p.target_object_id = OBJECT_ID(N'dbo.ReceteIstasyon')
+          AND p.predicate_type = 1 AND p.operation = 1
+    )
+    BEGIN
+        ALTER SECURITY POLICY dbo.SecurityPolicy_CompanyIsolation
+            ADD BLOCK PREDICATE dbo.fn_CompanyIsolationPredicate(CompanyId) ON dbo.ReceteIstasyon AFTER INSERT;
+    END;
+    IF NOT EXISTS (
+        SELECT 1 FROM sys.security_predicates p
+        WHERE p.object_id = OBJECT_ID(N'dbo.SecurityPolicy_CompanyIsolation')
+          AND p.target_object_id = OBJECT_ID(N'dbo.ReceteIstasyon')
+          AND p.predicate_type = 1 AND p.operation = 2
+    )
+    BEGIN
+        ALTER SECURITY POLICY dbo.SecurityPolicy_CompanyIsolation
+            ADD BLOCK PREDICATE dbo.fn_CompanyIsolationPredicate(CompanyId) ON dbo.ReceteIstasyon AFTER UPDATE;
+    END;
+END;
 GO
 
 /* Maliyet için mevcut ürün alış fiyatı kullanılır. */
