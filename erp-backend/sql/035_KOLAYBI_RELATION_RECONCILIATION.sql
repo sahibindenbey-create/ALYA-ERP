f97b172fc19285669eaebb03b6b927e34b72b89b
sahibindenbey-create <sahibindenbey@gmail.com>
2026-09-13 15:23:33 +0300

    fix: validate relation reconciliation prerequisites

--- erp-backend/sql/035_KOLAYBI_RELATION_RECONCILIATION.sql
+++ erp-backend/sql/035_KOLAYBI_RELATION_RECONCILIATION.sql
@@ -1,8 +1,10 @@
-/* 035 - KolayBi operasyonel kayıtları arasındaki ALYA ilişkilerini kurar. Veri silmez. */
+/* 035 V2 - KolayBi operasyonel kayıtları arasındaki ALYA ilişkilerini kurar. Veri silmez. */
 SET NOCOUNT ON;SET XACT_ABORT ON;
-IF OBJECT_ID(N'dbo.KolaybiBusinessMappings',N'U') IS NULL THROW 53501,N'Önce 033 migrationını çalıştırın.',1;
+IF OBJECT_ID(N'dbo.KolaybiBusinessMappings',N'U') IS NULL THROW 53501,N'Önce güncel 033 migrationını çalıştırın.',1;
 IF OBJECT_ID(N'dbo.BelgeBaglantilari',N'U') IS NULL THROW 53502,N'Önce 014 migrationını çalıştırın.',1;
 IF OBJECT_ID(N'dbo.CariDefterHareketleri',N'U') IS NULL THROW 53503,N'Önce 015 migrationını çalıştırın.',1;
+IF OBJECT_ID(N'dbo.FinansHareket',N'U') IS NULL THROW 53504,N'dbo.FinansHareket eksik. Güncel 033 V2 migrationını çalıştırın.',1;
+IF OBJECT_ID(N'dbo.CariHareket',N'U') IS NULL THROW 53505,N'dbo.CariHareket eksik. Güncel 034 migrationını çalıştırın.',1;
 IF COL_LENGTH(N'dbo.Faturalar',N'CariId') IS NULL EXEC sys.sp_executesql N'ALTER TABLE dbo.Faturalar ADD CariId INT NULL;';
 IF COL_LENGTH(N'dbo.Siparisler',N'CariId') IS NULL EXEC sys.sp_executesql N'ALTER TABLE dbo.Siparisler ADD CariId INT NULL;';
 IF COL_LENGTH(N'dbo.FinansHareket',N'CariId') IS NULL EXEC sys.sp_executesql N'ALTER TABLE dbo.FinansHareket ADD CariId INT NULL;';
@@ -15,5 +17,5 @@ IF EXISTS(SELECT 1 FROM sys.security_policies WHERE object_id=OBJECT_ID(N'dbo.Se
  IF NOT EXISTS(SELECT 1 FROM sys.security_predicates WHERE object_id=OBJECT_ID(N'dbo.SecurityPolicy_CompanyIsolation') AND target_object_id=OBJECT_ID(N'dbo.KolaybiLinkRuns') AND predicate_type=0) EXEC sys.sp_executesql N'ALTER SECURITY POLICY dbo.SecurityPolicy_CompanyIsolation ADD FILTER PREDICATE dbo.fn_CompanyIsolationPredicate(CompanyId) ON dbo.KolaybiLinkRuns;';
  IF NOT EXISTS(SELECT 1 FROM sys.security_predicates WHERE object_id=OBJECT_ID(N'dbo.SecurityPolicy_CompanyIsolation') AND target_object_id=OBJECT_ID(N'dbo.KolaybiLinkRuns') AND predicate_type=1 AND operation=1) EXEC sys.sp_executesql N'ALTER SECURITY POLICY dbo.SecurityPolicy_CompanyIsolation ADD BLOCK PREDICATE dbo.fn_CompanyIsolationPredicate(CompanyId) ON dbo.KolaybiLinkRuns AFTER INSERT;';
 END;
-PRINT N'035 KolayBi ilişki uzlaştırma altyapısı tamamlandı.';
+PRINT N'035 V2 KolayBi ilişki uzlaştırma altyapısı tamamlandı.';
 GO
