93576a35a4521983f2a2cc076bb7b785acca8591
sahibindenbey-create <sahibindenbey@gmail.com>
2026-09-14 18:43:11 +0300

    feat: register linked KolayBi invoice actions

--- erp-backend/company-context-hook.js
+++ erp-backend/company-context-hook.js
@@ -63,6 +63,7 @@ if (!express.application.__alyaKolaybiRoutesPatched) {
       const { install: installFull } = require('./kolaybi-full-sync');
       const { install: installFinans } = require('./kolaybi-finans');
       const { install: installEDocument } = require('./kolaybi-e-document');
+      const { install: installInvoiceActions } = require('./kolaybi-invoice-actions');
       const deps = { app: this, poolPromise, sql };
       installErp(deps);
       installInvoice(deps);
@@ -70,6 +71,7 @@ if (!express.application.__alyaKolaybiRoutesPatched) {
       installFull(deps);
       installFinans(deps);
       installEDocument(deps);
+      installInvoiceActions(deps);
       this.__alyaKolaybiRoutesInstalled = true;
     }
     return originalListen.apply(this, args);
